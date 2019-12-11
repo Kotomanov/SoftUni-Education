97519d1f27b700292878090bfeaaa6c29b32dbea
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2019-12-11 13:41:58 +0200

    ExampPrep

---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/TeisterMask/TeisterMask/Data/Models/EmployeeTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class EmployeeTask
+    {
+        // Anotations????
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public int TaskId { get; set; }
+
+        public Task Task { get; set; }
+
+
+    }
+}
---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/TeisterMask/TeisterMask/DataProcessor/ImportDto/ProjectImportDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Project")]
+    public class ProjectImportDto
+    {
+        [Required]
+        [MinLength(2), MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [XmlElement("OpenDate")]
+        public string OpenDate { get; set; } // string
+
+        [XmlElement("DueDate")]
+        public string DueDate { get; set; } // string ? //DateTime?
+
+        // collection of tasks 
+
+        [XmlArray("Tasks")]
+        public TaskImportDto[] Tasks { get; set; }
+    }
+}
