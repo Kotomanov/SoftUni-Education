049597c1896f8942675d125b691021e51d011484
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 20:46:06 +0200

    updates

---
+++ Education Entity Framework Core - Oct 2019/Entity Relations Exercise Problems and Solutions/EntityRelationsBettingFootballSystem/P03_FootballBetting.Data/Configurations/BetsConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P03_FootballBetting.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Configurations
+{
+    public class BetsConfiguration : IEntityTypeConfiguration<Bet>
+    {
+        public void Configure(EntityTypeBuilder<Bet> builder)
+        {
+            builder.HasKey(b => b.BetId);
+
+            builder.Property(b => b.Amount)
+                .IsRequired(true);
+
+            builder.Property(b => b.Prediction)
+                .IsRequired(true)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+
+            builder.Property(b => b.DateTime)
+                .IsRequired(true);
+
+            builder
+                .HasOne(b => b.User)
+                .WithMany(u => u.Bets)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(b => b.Game)
+                .WithMany(g => g.Bets)
+                .HasForeignKey(b => b.GameId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
