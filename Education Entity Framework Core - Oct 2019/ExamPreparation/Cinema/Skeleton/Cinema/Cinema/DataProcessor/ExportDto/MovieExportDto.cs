3d7a75cc7f2df1cd940b1752a8e5de64a5e3c2ce
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 23:54:32 +0200

    updates

---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/Cinema/Skeleton/Cinema/Cinema/DataProcessor/ExportDto/MovieExportDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cinema.DataProcessor.ExportDto
+{
+   public class MovieExportDto
+    {
+        public string MovieName { get; set; }
+
+        public string Rating { get; set; } // formatted
+
+        public string TotalIncomes { get; set; } // string formatted ? 
+
+        public ICollection<CustomersExportDto> Customers { get; set; }
+    }
+}
---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/Cinema/Skeleton/Cinema/Cinema/DataProcessor/ImportDto/HallSeatImportDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cinema.DataProcessor.ImportDto
+{
+    class HallSeatImportDto
+    {
+        [Required]
+        [MinLength(3), MaxLength(20)]
+        public string Name { get; set; }
+
+        public bool Is4Dx { get; set; }
+
+        public bool Is3D { get; set; }
+
+
+        [Range(1,int.MaxValue)]
+        public int Seats { get; set; }
+    }
+}
