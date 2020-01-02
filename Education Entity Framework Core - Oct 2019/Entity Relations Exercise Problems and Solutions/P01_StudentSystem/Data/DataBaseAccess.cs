0991747e04ba8e0a4b80086b84c729d2acbaabab
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 23:57:02 +0200

    updates

---
+++ Education Entity Framework Core - Oct 2019/Entity Relations Exercise Problems and Solutions/P01_StudentSystem/Data/DataBaseAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_StudentSystem.Data
+{
+    static class DataBaseAccess
+    {
+
+        internal static string DefaultConnection
+          = @"Server=DESKTOP-1SHT5A0\SQLEXPRESS; Database=SalesDatabase; Integrated Security = true";
+    }
+}
