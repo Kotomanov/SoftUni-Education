3f4b3ae611d81792f83562a23eb95d7ace682642
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 23:58:37 +0200

    Create DataConnection.cs

---
+++ Education Entity Framework Core - Oct 2019/Entity Relations Exercise Problems and Solutions/EntityRelationsBettingFootballSystem/P03_FootballBetting.Data/DataConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace P03_FootballBetting.Data
+{
+    static class DataConnection
+    {
+        public const string DefaultConnection
+            = "Server=DESKTOP-1SHT5A0\\SQLEXPRESS; Database= FootballBettingDb; Integrated Security = true";
+    }
+}
