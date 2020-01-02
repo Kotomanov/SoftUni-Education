251eba65821e4cb654de605f5376d8b0aa4149e6
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 23:52:09 +0200

    updates

---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/Exam/TeisterMask/DataProcessor/ExportDto/EmployeeTaskDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    public class EmployeeTaskDto
+    {
+        
+        public string Username { get; set; }
+
+        public ICollection<TaskEmployeeDto> Tasks { get; set; }
+    }
+}
