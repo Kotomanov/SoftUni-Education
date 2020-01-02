e517ab68fc3fda84e93303074b35d3e2cd14cad8
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-02 23:48:58 +0200

    updates

---
+++ Education Entity Framework Core - Oct 2019/ExamPreparation/Music/Solution/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+   public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=DESKTOP-1SHT5A0\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True";
+    }
+}
