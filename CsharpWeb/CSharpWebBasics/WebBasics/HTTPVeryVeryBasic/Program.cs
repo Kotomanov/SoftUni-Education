0f76224cf9dae3122d86d149152f4a53f89573db
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-17 01:26:32 +0200

    Update in the HTTP basic excercise

--- CsharpWeb/CSharpWebBasics/WebBasics/HTTPVeryVeryBasic/Program.cs
+++ CsharpWeb/CSharpWebBasics/WebBasics/HTTPVeryVeryBasic/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using System.Net;
 using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HTTPVeryVeryBasic
@@ -8,7 +11,53 @@ namespace HTTPVeryVeryBasic
     class Program
     {
         static async Task Main(string[] args)
-        {// request -get
+        {
+            const string NewLine = "\r\n";
+
+            TcpListener listener = new TcpListener(IPAddress.Loopback, 80);
+            listener.Start();
+
+            while (true) // Demon in Linux ... 
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                using (NetworkStream networkStream = client.GetStream())
+                {
+                    //Use buffer
+                    byte[] requestBytes = new byte[1000000];
+                    int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
+                    string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+                    string responseText = @"<form action ='/Account/Login' method = 'post' >
+                                 <input type = date name = 'date'/>
+                                 <input type = text name = 'username'/>
+                                 <input type = password = 'password'/>
+                                 <input type = submit value = 'Login'/>
+                                           </form>";
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+
+                    string response = "HTTP/1.0 307 Relocation" + NewLine +
+                        "Server: BGMapsServer/1.0" + NewLine +
+                        "Content - Type: text/html" + NewLine +
+                        "Location: https://duckduckgo.com" + NewLine +
+                        //"Content - Disposition: attachment; filename=download.html" + NewLine +
+                        "Content - Lenght: " + responseText.Length + NewLine
+                         + NewLine +
+                         responseText;
+
+
+                    networkStream.Write(responseBytes, 0, responseBytes.Length);
+
+
+                    Console.WriteLine(request);
+                    Console.WriteLine(new string('=', 60));
+                }
+
+            }
+
+        }
+
+        public static async Task HttpRequest()
+        {
+            // request -get
             HttpClient client = new HttpClient();
             HttpResponseMessage message = await client.GetAsync("https://www.bgmaps.com/");
             string result = await message.Content.ReadAsStringAsync();
@@ -18,6 +67,7 @@ namespace HTTPVeryVeryBasic
             //request -post
 
             //request - response 
+
         }
     }
 }
