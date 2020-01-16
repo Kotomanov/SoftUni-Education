3e8cc5c091ecceaeb114a91ae2c605df97ecab08
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-16 12:55:43 +0200

    Update Program.cs

--- CsharpWeb/CSharpWebBasics/WebBasics/HTTPVeryVeryBasic/Program.cs
+++ CsharpWeb/CSharpWebBasics/WebBasics/HTTPVeryVeryBasic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -7,11 +8,16 @@ namespace HTTPVeryVeryBasic
     class Program
     {
         static async Task Main(string[] args)
-        {
+        {// request -get
             HttpClient client = new HttpClient();
             HttpResponseMessage message = await client.GetAsync("https://www.bgmaps.com/");
             string result = await message.Content.ReadAsStringAsync();
             Console.WriteLine(result);
+            File.WriteAllText("index.html", result);
+
+            //request -post
+
+            //request - response 
         }
     }
 }
