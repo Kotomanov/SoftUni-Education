1223196b24c34c052ef51e53e9b674a9e2bb3fe8
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-01-20 11:22:38 +0200

    update

--- CsharpWeb/CSharpWebBasics/WebBasics/AsynchronousProgramming/Program.cs
+++ CsharpWeb/CSharpWebBasics/WebBasics/AsynchronousProgramming/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 
 namespace AsynchronousProgramming
 {
@@ -8,11 +9,98 @@ namespace AsynchronousProgramming
         static int counter;
 
         static void Main(string[] args)
-        {
+        {//can  be done wuth annonimous method
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    // Console.WriteLine(1);
+                }
+            }).Start();
 
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    // Console.WriteLine(5);
+                }
+            }).Start();
+
+            // parallel  feeding an account 
+
+            decimal amount = 0;
+
+            object lockObject = new object();
+
+            var thread1 = new Thread(
+                () =>
+                {
+                    for (int i = 0; i < 100_000; i++)
+                    {
+                        lock (lockObject)
+                        {
+                            amount++;
+                        }
+
+                    }
+                }
+                );
+            thread1.Start();
+
+            var thread2 = new Thread(
+                () =>
+                {
+                    for (int i = 0; i < 100_000; i++)
+                    {
+                        lock (lockObject)
+                        {
+                            amount++;
+                        }
+
+                    }
+                }
+                );
+            thread2.Start();
+
+            var thread3 = new Thread(
+                () =>
+                {
+                    for (int i = 0; i < 100_000; i++)
+                    {
+                        lock (lockObject)
+                        {
+                            amount++;
+                        }
+
+                    }
+                }
+                );
+            thread3.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+
+            Console.WriteLine(amount);
+
+            // can be instantiazed : 
+            Thread thread = new Thread(MyThreadMainMethod);
+            thread.Start();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                //Console.WriteLine(line.ToUpper());
+            }
+
+        }
+
+        private static void MyThreadMainMethod()
+        {
             var time = Stopwatch.StartNew();
-            Console.WriteLine(CountOfPrimeNumbersInRange(1, 1_000_000));
-            Console.WriteLine(time.Elapsed);
+            //Console.WriteLine(CountOfPrimeNumbersInRange(1, 1_000_000));
+            //Console.WriteLine(time.Elapsed);
         }
 
         private static int CountOfPrimeNumbersInRange(int from, int to)
