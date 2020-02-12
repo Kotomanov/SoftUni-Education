90f62a4066e246944ec8cf40ebdd20d2ba5f67bf
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-02-12 19:47:06 +0200

    Updates in the solution

---
+++ CsharpWeb/CSharpWebBasics/BasicsExamPrep/IRunes/Skeleton/IRunes/IRunes.App/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace IRunes.App.Models
+{
+    public class User
+    {
+        public User()
+        {
+            this.Id = Guid.NewGuid().ToString(); 
+        }
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
