ac6c166da51a04116bd75443e6575d9355ed7946
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-02-18 21:56:21 +0200

    updates

---
+++ SoftUniada/Softuniada Training Contest (Sandbox)/Softuniada SandBox/Min3Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Min3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < counter; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+            numbers.Sort();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (i == 3)
+                {
+                    return;
+                }
+
+                Console.WriteLine(numbers[i]);
+
+            }
+        }
+    }
+}
