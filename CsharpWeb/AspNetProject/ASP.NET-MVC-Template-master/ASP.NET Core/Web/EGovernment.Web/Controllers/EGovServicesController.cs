8d7becb79aea8792528e93a6db0b7b053efba127
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2020-04-17 01:16:08 +0300

    New View added

---
+++ CsharpWeb/AspNetProject/ASP.NET-MVC-Template-master/ASP.NET Core/Web/EGovernment.Web/Controllers/EGovServicesController.cs
@@ -0,0 +1,12 @@
+﻿namespace EGovernment.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class EGovServicesController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
