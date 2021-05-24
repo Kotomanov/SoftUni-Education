c49978a7f19a0647e82e64cbdf0c6b7a5ea228d5
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-05-25 01:50:26 +0300

    Update CinemaTickets.js

--- JS/JSBasics/NestedLoops/CinemaTickets.js
+++ JS/JSBasics/NestedLoops/CinemaTickets.js
@@ -1,37 +1,64 @@
 function cinemaTickets(params) {
-    let studentCount = 0;
-    let kidCount = 0;
-    let standardCount = 0;
+    let studentTicketsCount = 0;
+    let kidTicketsCount = 0;
+    let standardTicketsCount = 0;
     let seatsOcupied = 0;
     let helpCount = 0;
+    let totalTicketsCount = 0;
 
     for (let i = 0; i < params.length; i++) {
 
         if (params[i] === "Finish") {
             break;
         }
-
         else if (params[i] === "End") {
-            studentCount = 0;
-            kidCount = 0;
-            standardCount = 0;
+            // extract the movie name and % ocupancy
+            //movie name
+            // % formula
             seatsOcupied = 0;
             helpCount = 0;
+            break; //?
         }
+        else if (params[i] === "standard") {
+            standardTicketsCount++;
+            seatsOcupied++;
+            totalTicketsCount++;
+        }
+        else if (params[i] === "student") {
+            studentTicketsCount++;
+            seatsOcupied++;
+            totalTicketsCount++;
+        }
+        else if (params[i] === "kid") {
+            kidTicketsCount++;
+            seatsOcupied++;
+            totalTicketsCount++;
+        }
+        else if (isNaN(params[i])) {
+            //moviename
+        }
+        else if (!isNaN(params[i])) {
+            //seatsinhall
+        }
+
+
 
         while (params[i] !== "End") {
             if (helpCount == 0) {
-
+                //movieName
+            } else if (helpCount == 1) {
+                //seatsCount
             }
-
-            else if(helpCount == 0){
-
-            }
+            helpCount++;
         }
 
     }
 
-
+    //extract the total of each type of tickets 
+    console.log(`Total tickets: ${totalTicketsCount}`);
+    console.log(`${((studentTicketsCount/totalTicketsCount)*100).toFixed(2)}% student tickets.`);
+    console.log(`${((standardTicketsCount/totalTicketsCount)*100).toFixed(2)}% standard tickets.`);
+    console.log(`${((studentTicketsCount/totalTicketsCount)*100).toFixed(2)}% kids tickets.`);
 
 }
 
