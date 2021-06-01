60b90b26f7c91fbc39529867d7902ab9bdc292ca
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-06-02 01:02:37 +0300

    Update LettersCombinations.js

---
+++ JS/JSBasics/NestedLoops/LettersCombinations.js
@@ -0,0 +1,24 @@
+function lettersCombinations(params) {
+    let firstLetter = params[0];
+    let secondLetter = params[1];
+    let thirdLetter = params[2];
+    let counter = 0; 
+
+    for (let f = a; f <= z; f++) {
+        for (let s = f; s <= z; s++) {
+            for (let t = a; t <= z; t++) {
+                
+
+            }
+
+        }
+
+    }
+
+}
+
+lettersCombinations(["a", "c", "b"]);
+
+lettersCombinations(["f", "k", "h"]);
+
+lettersCombinations(["a", "c", "z"]);
