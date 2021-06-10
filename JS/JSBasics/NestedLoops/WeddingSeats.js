d461deda0aa485dffce81dd0bc9d0895717227f0
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-06-11 01:06:17 +0300

    Update WeddingSeats.js

--- JS/JSBasics/NestedLoops/WeddingSeats.js
+++ JS/JSBasics/NestedLoops/WeddingSeats.js
@@ -3,21 +3,41 @@ function weddingSeats(params) {
     let firstSectorRowCount = Number(params[1]);
     let oddRowSeatCount = Number(params[2]);
     let outcome = "";
+    let counter = 0;
     //101 - 132
-    for (let s = 101; s <= lastSector; s++) {
-        for (let row = 0; row <= firstSectorRowCount; row++) {
-            for (let seat = 0; seat <= oddRowSeatCount; seat++) {
-                outcome += String.fromCharCode(s) + row.toString() + seat.toString();
-                console.log(outcome);
-                outcome = "";
+    for (let s = 65; s <= lastSector; s++) {
+
+        for (let row = 1; row <= firstSectorRowCount; row++) {
+            if (row % 2 == 0) {
+                //seats+=2
+                for (let seats = 97; seats <= 97 + oddRowSeatCount + 2; seats++) {
+                    //convert to letter again
+                    outcome += String.fromCharCode(s) + row.toString() + String.fromCharCode(seats);
+                    counter++;
+                    break;
+                }
+            } else {
+                for (let seats = 97; seats <= 97 + oddRowSeatCount; seats++) {
+                    outcome += String.fromCharCode(s) + row.toString() + String.fromCharCode(seats);
+                    counter++;
+                    break;
+                }
 
             }
-            //increase rows
+            console.log(outcome);
+            outcome = "";
+
+
         }
 
+        firstSectorRowCount++; // next sector has the rows ++; 
+
+
+
     }
+    console.log(counter);
 }
 
 weddingSeats(["B", "3", "2"]);
 
-weddingSeats(["C", "4", "2"]);
+//weddingSeats(["C", "4", "2"]);
