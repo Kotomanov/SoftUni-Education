1cedc42bfddb98335092c5c3417bba3915295ba4
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-06-12 01:24:26 +0300

    Create SafePasswordsGenerator.js

---
+++ JS/JSBasics/NestedLoops/SafePasswordsGenerator.js
@@ -0,0 +1,14 @@
+function safePasswordsGenerator(params) {
+    let firstNumber = Number(params[0]); //35-55
+    let secondNumber = Number(params[1]); //64-96
+    let maxCount = Number(params[2]);
+    let outcome = "";
+    //ABxyBA x=1 to a , y = 1 to b 
+
+
+
+    console.log(outcome)
+}
+
+safePasswordsGenerator(["2", "3", "10"]);
+safePasswordsGenerator(["20", "50", "10"]);
