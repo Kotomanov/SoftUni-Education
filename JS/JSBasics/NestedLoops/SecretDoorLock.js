0c17a4019eb3a6ef30d48824f42ea350058b2c1a
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-06-22 01:16:35 +0300

    new problem resolution in progress

--- JS/JSBasics/NestedLoops/SecretDoorLock.js
+++ JS/JSBasics/NestedLoops/SecretDoorLock.js
@@ -3,19 +3,16 @@ function secretDoorLock(params) {
     let decimals = Number(params[1]);
     let units = Number(params[2]);
 
-    for (let u = 1; u <= hundreds; u++) { //units
-        for (let d = 1; d <= decimals; d++) { //decimals
-            for (let h = 1; h <= units; h++) { //hundreds
+    for (let u = 1; u <= hundreds; u++) {
+        for (let d = 1; d <= decimals; d++) {
+            for (let h = 1; h <= units; h++) {
                 if (u % 2 == 0 && h % 2 == 0) {
                     if (d == 2 || d == 3 || d == 5 || d == 7) {
                         console.log(`${u} ${d} ${h}`);
                     }
                 }
-
             }
-
         }
-
     }
 
 }
