49466f3d0bdaaeedf553199cebd1df98c512b443
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-12 01:29:04 +0300

    Update BestPlayer.js
    Problem solved

--- JS/JSBasics/WhileLoops/BestPlayer.js
+++ JS/JSBasics/WhileLoops/BestPlayer.js
@@ -1,31 +1,31 @@
 function bestPlayer(params) {
     let index = 0;
-    let playerName = params[index];
-        let goalsCount = Number(params[index+1]);
+    let goalsCount = -1;
     let bestPlayerName = "";
-    let bestPlayerScore = -1; 
+    let bestPlayerScore = -1;
     let outcome = "";
-    
 
-    while (playerName !== "END") {
-        
-        if (goalsCount > bestPlayerScore) {
-            bestPlayerScore =  goalsCount;
-            bestPlayerName = playerName;
+    while (params[index] !== "END") {
+
+        if (index % 2 == 0) {
+            goalsCount = Number(params[index + 1]);
+            if (goalsCount > bestPlayerScore) {
+                bestPlayerScore = goalsCount;
+                bestPlayerName = params[index];
+            }
         }
 
-        if (bestPlayerScore>=10) {
-            break;//?
+        if (bestPlayerScore >= 10) {
+            break;
         }
-
         index++;
     }
     console.log(`${bestPlayerName} is the best player!`);
-    outcome +=`He has scored ${bestPlayerScore} goals `;
-    if (bestPlayerScore>=3) {
-        outcome +="and made a hat-trick !!!";
-        //console.log(`He has scored ${bestPlayerScore} goals and made a hat-trick !!!`);
-    } 
+    outcome += `He has scored ${bestPlayerScore} goals.`;
+    if (bestPlayerScore >= 3) {
+        outcome = `He has scored ${bestPlayerScore} goals and made a hat-trick !!!`;
+    }
+
     console.log(outcome);
 }
 
@@ -33,4 +33,8 @@ bestPlayer(["Neymar", "2", "Ronaldo", "1", "Messi", "3", "END"]);
 
 bestPlayer(["Neymar", "2", "Ronaldo", "1", "Messi", "10"]);
 
-bestPlayer(["Silva", "5", "Harry", "Kane", "10"]);
+bestPlayer(["Silva", "5", "Kane", "10"]); //?
+
+bestPlayer(["Neymar", "1", "Ronaldo", "2", "Messi", "2", "END"]);
+
+bestPlayer(["Neymar", "2", "Ronaldo", "11"]);
