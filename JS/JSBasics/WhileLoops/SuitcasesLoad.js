317fc431cda4bf9d574c118c51e315fb4f30e3ce
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-13 02:02:00 +0300

    Update SuitcasesLoad.js

---
+++ JS/JSBasics/WhileLoops/SuitcasesLoad.js
@@ -0,0 +1,38 @@
+function suitcasesLoad(params) {
+    let capacity = Number(params[0]);
+    let index = 1;
+    //let command = params[index];
+    let outcome = "";
+    let counter = 0;
+
+
+
+    while (params[index] !== "End") {
+
+        capacity -= Number(params[index]);
+        index++;
+
+        if (capacity < 0) {
+            break;
+        }
+        counter++;
+    }
+
+    if (capacity >= 0) {
+        outcome = "Congratulations! All suitcases are loaded!";
+    }
+    else {
+        outcome = "No more space!";
+    }
+    console.log(outcome);
+    console.log(`Statistic: ${counter} suitcases loaded.`);
+}
+
+suitcasesLoad(["550", "100", "252", "72", "End"]);
+
+suitcasesLoad(["700.5", "180", "340.6", "126", "220"]);
+
+suitcasesLoad(["1200.2", "260", "380.5", "125.6", "305", "End"]);
+
+
+
