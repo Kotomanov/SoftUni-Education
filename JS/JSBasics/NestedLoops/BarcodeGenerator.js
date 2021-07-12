62315839e3108cb2f7fd6a20da0799fc93e3abca
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-13 00:16:40 +0300

    Update BarcodeGenerator.js

--- JS/JSBasics/NestedLoops/BarcodeGenerator.js
+++ JS/JSBasics/NestedLoops/BarcodeGenerator.js
@@ -3,27 +3,26 @@ function barcodeGenerator(params) {
     let secondNumber = Number(params[1]);
     let counter = 0;
     let output = "";
+    let letterToNumber = 0;
     for (let f = firstNumber; f <= secondNumber; f++) {
         let numberCheck = f.toString();
-        let letterToNumber = 0;
+        counter = 0;
         for (let i = 0; i < numberCheck.length; i++) { //numberCheck.length
             letterToNumber = Number(numberCheck[i]);
             if (letterToNumber % 2 !== 0) {
                 counter++;
                 if (counter == 4) {
-                    counter = 0;
-                    output += `${numberCheck} + " "`;
-
-
+                    output += `${numberCheck} `;
                 }
             }
         }
 
     }
+    console.log(output);
 }
 
-barcodeGenerator(["2345", "6789"]);
+//barcodeGenerator(["2345", "6789"]);
 
 //barcodeGenerator(["3256", "6579"]);
 
-//barcodeGenerator(["1365", "5877"]);
+barcodeGenerator(["1365", "5877"]);
