99faf3844f2ba5d384ee35f573861992296ce055
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-13 00:38:37 +0300

    Update BarcodeGenerator.js
    Problem solved

--- JS/JSBasics/NestedLoops/BarcodeGenerator.js
+++ JS/JSBasics/NestedLoops/BarcodeGenerator.js
@@ -1,28 +1,34 @@
 function barcodeGenerator(params) {
-    let firstNumber = Number(params[0]);
-    let secondNumber = Number(params[1]);
-    let counter = 0;
+    let firstNumber = params[0];
+    let secondNumber = params[1];
     let output = "";
-    let letterToNumber = 0;
-    for (let f = firstNumber; f <= secondNumber; f++) {
-        let numberCheck = f.toString();
-        counter = 0;
-        for (let i = 0; i < numberCheck.length; i++) { //numberCheck.length
-            letterToNumber = Number(numberCheck[i]);
-            if (letterToNumber % 2 !== 0) {
-                counter++;
-                if (counter == 4) {
-                    output += `${numberCheck} `;
+    let firstStart = Number(firstNumber[0]);
+    let firstEnd = Number(secondNumber[0]);
+    let secondStart = Number(firstNumber[1]);
+    let secondEnd = Number(secondNumber[1]);;
+    let thirdStart = Number(firstNumber[2]);
+    let thirdEnd = Number(secondNumber[2]);;
+    let fourthStart = Number(firstNumber[3]);
+    let fourthEnd = Number(secondNumber[3]);;
+
+
+    for (let f = firstStart; f <= firstEnd; f++) {
+        for (let s = secondStart; s <= secondEnd; s++) {
+            for (let t = thirdStart; t <= thirdEnd; t++) {
+                for (let l = fourthStart; l <= fourthEnd; l++) {
+                    if (f % 2 !== 0 && s % 2 !== 0 && t % 2 !== 0 && l % 2 !== 0) {
+                        output += `${f}${s}${t}${l} `;
+                    }
                 }
             }
         }
-
     }
+
     console.log(output);
 }
 
-//barcodeGenerator(["2345", "6789"]);
+barcodeGenerator(["2345", "6789"]);
 
-//barcodeGenerator(["3256", "6579"]);
+barcodeGenerator(["3256", "6579"]);
 
 barcodeGenerator(["1365", "5877"]);
