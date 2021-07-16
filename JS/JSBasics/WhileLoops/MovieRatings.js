1fd04e25cf77af262aa2efe404d9803680a377a3
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-17 02:01:29 +0300

    Update MovieRatings.js

--- JS/JSBasics/WhileLoops/MovieRatings.js
+++ JS/JSBasics/WhileLoops/MovieRatings.js
@@ -1,7 +1,7 @@
 function movieRatings(params) {
     let movieCount = Number(params[0]);
     let index = 1;
-    let highestRating = -1;
+    let highestRating = 0;
     let lowestRating = 11;
     let bestMovieTitle = "";
     let lastMovieTitle = "";
@@ -9,11 +9,11 @@ function movieRatings(params) {
     let averageRating = 0;
     while (index <= movieCount * 2) {
         if (index % 2 == 0) {
-            if (Number(params[index]) > highestRating) {
+            if (Number(params[index]) >= highestRating) {
                 highestRating = Number(params[index]);
                 bestMovieTitle = params[index - 1];
             }
-            if (Number(params[index]) < highestRating) {
+            else  { 
                 lowestRating = Number(params[index]);
                 lastMovieTitle = params[index - 1];
             }
@@ -22,7 +22,7 @@ function movieRatings(params) {
         }
         index++;
     }
-    averageRating = totalRating / movieCount
+    averageRating = totalRating / movieCount;
     console.log(`${bestMovieTitle} is with highest rating: ${highestRating.toFixed(1)}`);
     console.log(`${lastMovieTitle} is with lowest rating: ${lowestRating.toFixed(1)}`);
     console.log(`Average rating: ${averageRating.toFixed(1)}`);
