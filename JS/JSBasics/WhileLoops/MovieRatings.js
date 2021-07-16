e23d27a9b68daf61af4131fb7996666d2fe65c97
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-17 01:37:35 +0300

    Update MovieRatings.js

--- JS/JSBasics/WhileLoops/MovieRatings.js
+++ JS/JSBasics/WhileLoops/MovieRatings.js
@@ -1,28 +1,25 @@
 function movieRatings(params) {
     let movieCount = Number(params[0]);
     let index = 1;
-    let highestRating = 0;
-    let lowestRating = 0;
+    let highestRating = -1;
+    let lowestRating = 11;
     let bestMovieTitle = "";
     let lastMovieTitle = "";
     let totalRating = 0;
     let averageRating = 0;
     while (index <= movieCount * 2) {
-        if (index % 2 == 0) { //  movierating
+        if (index % 2 == 0) {
             if (Number(params[index]) > highestRating) {
                 highestRating = Number(params[index]);
-                totalRating += Number(params[index]);
-                bestMovieTitle = params[index + 1];
+                bestMovieTitle = params[index - 1];
             }
             if (Number(params[index]) < highestRating) {
                 lowestRating = Number(params[index]);
-                totalRating += Number(params[index]);
-                lastMovieTitle = params[index + 1];
+                lastMovieTitle = params[index - 1];
             }
 
+            totalRating += Number(params[index]);
         }
-
-
         index++;
     }
     averageRating = totalRating / movieCount
@@ -31,6 +28,6 @@ function movieRatings(params) {
     console.log(`Average rating: ${averageRating.toFixed(1)}`);
 }
 
-//movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
+movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
 
 movieRatings(["3", "Interstellar", "8.5", "Dangal", "8.3", "Green Book", "8.2"]);
