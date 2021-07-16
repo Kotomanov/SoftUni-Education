a93a6d22a0b0623bbd4b1dcfa6b281b7c2a882c2
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-17 01:19:45 +0300

    Update MovieRatings.js

---
+++ JS/JSBasics/WhileLoops/MovieRatings.js
@@ -0,0 +1,36 @@
+function movieRatings(params) {
+    let movieCount = Number(params[0]);
+    let index = 1;
+    let highestRating = 0;
+    let lowestRating = 0;
+    let bestMovieTitle = "";
+    let lastMovieTitle = "";
+    let totalRating = 0;
+    let averageRating = 0;
+    while (index <= movieCount * 2) {
+        if (index % 2 == 0) { //  movierating
+            if (Number(params[index]) > highestRating) {
+                highestRating = Number(params[index]);
+                totalRating += Number(params[index]);
+                bestMovieTitle = params[index + 1];
+            }
+            if (Number(params[index]) < highestRating) {
+                lowestRating = Number(params[index]);
+                totalRating += Number(params[index]);
+                lastMovieTitle = params[index + 1];
+            }
+
+        }
+
+
+        index++;
+    }
+    averageRating = totalRating / movieCount
+    console.log(`${bestMovieTitle} is with highest rating: ${highestRating.toFixed(1)}`);
+    console.log(`${lastMovieTitle} is with lowest rating: ${lowestRating.toFixed(1)}`);
+    console.log(`Average rating: ${averageRating.toFixed(1)}`);
+}
+
+//movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
+
+movieRatings(["3", "Interstellar", "8.5", "Dangal", "8.3", "Green Book", "8.2"]);
