f1d9487c200aecefb82d964a37455bc6e1e76ce9
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-18 02:08:51 +0300

    Update TheMostPowerfulWord.js

--- JS/JSBasics/NestedLoops/TheMostPowerfulWord.js
+++ JS/JSBasics/NestedLoops/TheMostPowerfulWord.js
@@ -3,36 +3,43 @@ function theMostPowerfulWord(params) {
     let outcome = "";
     let wordPower = 0;
     let sumOfChars = 0;
+    let isVowel = false;
 
     while (params[index] != "End of words") {
         for (let i = 0; i < params[index].length; i++) {
-            sumOfChars += params[index][i].charCodeAt(0);
+
             if (params[index][0] == 'a' ||
                 params[index][0] == 'e' ||
                 params[index][0] == 'i' ||
                 params[index][0] == 'o' ||
                 params[index][0] == 'u' ||
+                params[index][0] == 'y' ||
                 params[index][0] == 'A' ||
                 params[index][0] == 'E' ||
                 params[index][0] == 'I' ||
                 params[index][0] == 'O' ||
-                params[index][0] == 'U') {//'a', 'e', 'i', 'o', 'u', 'y'  or capital 
-                sumOfChars *= params[index].length;
+                params[index][0] == 'U' ||
+                params[index][0] == 'Y') {
+                isVowel = true;
             }
-            else {
-                sumOfChars /= params[index].length;
-                sumOfChars = Math.floor(sumOfChars);
+            sumOfChars += params[index][i].charCodeAt(0);
 
-            }
         }
 
-
+        if (isVowel == true) {
+            sumOfChars *= params[index].length;
+        }
+        else {
+            sumOfChars /= params[index].length;
+            sumOfChars = Math.floor(sumOfChars);
+        }
 
         if (sumOfChars > wordPower) {
             wordPower = sumOfChars;
             outcome = params[index];
             sumOfChars = 0;
         }
+
         index++;
 
     }
