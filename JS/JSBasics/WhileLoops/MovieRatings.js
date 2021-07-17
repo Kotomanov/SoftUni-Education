8364bafc2e9df12e6b7df66e0cb294791171823a
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-18 00:52:37 +0300

    Update MovieRatings.js
    problem solved

--- JS/JSBasics/WhileLoops/MovieRatings.js
+++ JS/JSBasics/WhileLoops/MovieRatings.js
@@ -1,7 +1,7 @@
 function movieRatings(params) {
     let movieCount = Number(params[0]);
     let index = 1;
-    let highestRating = 0;
+    let highestRating = -1;
     let lowestRating = 11;
     let bestMovieTitle = "";
     let lastMovieTitle = "";
@@ -9,11 +9,11 @@ function movieRatings(params) {
     let averageRating = 0;
     while (index <= movieCount * 2) {
         if (index % 2 == 0) {
-            if (Number(params[index]) >= highestRating) {
+            if (Number(params[index]) > highestRating) {
                 highestRating = Number(params[index]);
                 bestMovieTitle = params[index - 1];
             }
-            else  { 
+             if (Number(params[index]) < lowestRating) { 
                 lowestRating = Number(params[index]);
                 lastMovieTitle = params[index - 1];
             }
@@ -28,6 +28,8 @@ function movieRatings(params) {
     console.log(`Average rating: ${averageRating.toFixed(1)}`);
 }
 
-movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
+//movieRatings(["5", "A Star is Born", "7.8", "Creed 2", "7.3", "Mary Poppins", "7.2", "Vice", "7.2", "Captain Marvel", "7.1"]);
 
-movieRatings(["3", "Interstellar", "8.5", "Dangal", "8.3", "Green Book", "8.2"]);
+//movieRatings(["3", "Interstellar", "8.5", "Dangal", "8.3", "Green Book", "8.2"]);
+
+movieRatings(["3", "Interstellar", "1.1", "Dangal", "1.3", "Green Book", "9.9"]);
