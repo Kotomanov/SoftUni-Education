b8e9e9cd20d1cbe5a612b92ef279d987c05c2cb3
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-19 01:21:23 +0300

    Update NameGame.js

---
+++ JS/JSBasics/NestedLoops/NameGame.js
@@ -0,0 +1,42 @@
+function nameGame(params) {
+    let index = 0;
+    let name = "";
+    let playerPoints = 0;
+    let bestPlayerPoints = 0;
+    let playerName = "";
+    let bestPlayerName = "";
+    //let counter = 0;
+
+    while (params[index] !== "Stop") {
+        if (isNaN(params[index])) { // is name
+            name = params[index];
+            for (let i = 0; i < name.length; i++) { //Ivan
+                index++;
+                if (name[i].charCodeAt(0) == Number(params[index])) {
+                    playerPoints += 10
+                }
+                else {
+                    playerPoints += 2;
+                }
+                playerName = name;
+
+            }
+
+            if (bestPlayerPoints <= playerPoints) {
+
+                bestPlayerPoints = playerPoints;
+                bestPlayerName = playerName;
+                playerPoints = 0;
+                
+            }
+            index++;
+        }
+    }
+
+    console.log(`The winner is ${bestPlayerName} with ${bestPlayerPoints} points!`);
+}
+
+nameGame(["Ivan", "73", "20", "98", "110", "Ivo", "80", "65", "87", "Stop"]);
+
+
+nameGame(["Pesho", "124", "34", "111", "97", "99", "Gosho", "98", "124", "88", "76", "18", "Stop"]);
