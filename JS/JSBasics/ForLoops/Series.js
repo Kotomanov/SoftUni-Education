0f4b308cfc169704a664cc3e55f3ecb61581e6bb
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-27 02:14:33 +0300

    Update Series.js
    problem solved

--- JS/JSBasics/ForLoops/Series.js
+++ JS/JSBasics/ForLoops/Series.js
@@ -1,8 +1,37 @@
 function series(params) {
-let budget = Number(params[0]);
-let seriesCount = Number(params[1]);
+    let budget = Number(params[0]);
+    let seriesCount = Number(params[1]);
+    let seriePrice = 0;
 
+    for (let index = 2; index < params.length; index++) {
+        if (index % 2 == 0) { // name 
+            
+            if (params[index] == "Thrones") {
+                budget -= 0.5*(Number(params[index + 1]));
+            } else if (params[index] == "Lucifer") {
+                budget -= 0.6*(Number(params[index + 1]));
+            }
+            else if (params[index] == "Protector") {
+                budget -= 0.7*(Number(params[index + 1]));
+            }
+            else if (params[index] == "TotalDrama") {
+                budget -= 0.8*(Number(params[index + 1]));
+            }
+            else if (params[index] == "Area") {
+                budget -= 0.9*(Number(params[index + 1]));
+            }
+            else{
+                budget -= Number(params[index + 1]);
+            }
+        }
 
+    }
+
+    if (budget >= 0) {
+        console.log(`You bought all the series and left with ${budget.toFixed(2)} lv.`);
+    } else {
+        console.log(`You need ${(budget * -1).toFixed(2)} lv. more to buy the series!`);
+    }
 
 }
 
