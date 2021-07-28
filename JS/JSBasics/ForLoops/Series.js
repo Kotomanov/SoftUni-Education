aa6a948658155ea06b61e3ca6118a94169dfd33c
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-07-29 01:12:13 +0300

    new problem started

--- JS/JSBasics/ForLoops/Series.js
+++ JS/JSBasics/ForLoops/Series.js
@@ -1,30 +1,26 @@
 function series(params) {
     let budget = Number(params[0]);
-    let seriesCount = Number(params[1]);
-    let seriePrice = 0;
 
     for (let index = 2; index < params.length; index++) {
-        if (index % 2 == 0) { // name 
-            
+        if (index % 2 == 0) {
             if (params[index] == "Thrones") {
-                budget -= 0.5*(Number(params[index + 1]));
+                budget -= 0.5 * (Number(params[index + 1]));
             } else if (params[index] == "Lucifer") {
-                budget -= 0.6*(Number(params[index + 1]));
+                budget -= 0.6 * (Number(params[index + 1]));
             }
             else if (params[index] == "Protector") {
-                budget -= 0.7*(Number(params[index + 1]));
+                budget -= 0.7 * (Number(params[index + 1]));
             }
             else if (params[index] == "TotalDrama") {
-                budget -= 0.8*(Number(params[index + 1]));
+                budget -= 0.8 * (Number(params[index + 1]));
             }
             else if (params[index] == "Area") {
-                budget -= 0.9*(Number(params[index + 1]));
+                budget -= 0.9 * (Number(params[index + 1]));
             }
-            else{
+            else {
                 budget -= Number(params[index + 1]);
             }
         }
-
     }
 
     if (budget >= 0) {
