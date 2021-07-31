6a6225f1261266dd67bdd719ccbb0e9afb3e1631
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-01 02:05:13 +0300

    Update HighJump.js

--- JS/JSBasics/ForLoops/HighJump.js
+++ JS/JSBasics/ForLoops/HighJump.js
@@ -1,40 +1,26 @@
 function highJump(params) {
-    let targetedHeigth = Number(params[0]); // record
-    let startingHeigth = targetedHeigth - 30; //starts 30 cm under the record
+    let targetedHeigth = Number(params[0]);
+    let startingHeigth = targetedHeigth - 30;
     let jumpsCount = 0;
     let failsCount = 0;
-    let successHeigth = 0;
-    let failHeigth = 0;
-    let failHeigthCm = 0;
-    let isSuccessful = false; 
 
     for (let i = 1; i <= params.length; i++) {
-        if (Number(params[i]) <= startingHeigth) { // fails as under the minimum
-            failHeigth = Number(params[i]);
-            //При три неуспешни скока на една и съща височина
-            failsCount++;
-            if (failsCount >= 3) {
-                i = params.length + 1; //??
-                //break; //??? because of jumpsCount..
-            }
-            //3 times fail at the same heigth 
-        }
-        else { // he manages to jump above the target height
-            startingHeigth += 5;//increase by 5 cm
-            failsCount = 0; //??
-            successHeigth = Number(params[i]);
-            if (startingHeigth<= successHeigth) { //has jumped successfully
-                
-            }
-        }
-
         jumpsCount++;
-    }
-
-    if (isSuccessful) { // success 
-        console.log(`Tihomir succeeded, he jumped over ${targetedHeigth} cm after ${jumpsCount} jumps.`);
-    } else { // fails
-        console.log(`Tihomir failed at ${failHeigth}cm after ${jumpsCount} jumps.`);
+        if (Number(params[i]) <= startingHeigth) {
+            failsCount++;
+            if (failsCount == 3) {
+                console.log(`Tihomir failed at ${Number(params[i])}cm after ${jumpsCount} jumps.`);
+                break;
+            }
+        }
+        else {
+            if (Number(params[i]) > targetedHeigth) {
+                console.log(`Tihomir succeeded, he jumped over ${targetedHeigth}cm after ${jumpsCount} jumps.`);
+                break;
+            }
+            startingHeigth += 5;
+            failsCount = 0;
+        }
     }
 }
 
