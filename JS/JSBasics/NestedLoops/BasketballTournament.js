65b91c145f94de3eb7c92cd84bf48a2a8c68a1d1
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-02 02:18:07 +0300

    Update BasketballTournament.js

---
+++ JS/JSBasics/NestedLoops/BasketballTournament.js
@@ -0,0 +1,18 @@
+function basketballTournament(params) {
+    let index = 0; 
+    let teamName = params[index];
+    
+
+    while (condition) {
+        
+    }
+
+
+
+}
+
+basketballTournament(["Dunkers","2","75","65","56","73","Fire Girls","3","67","34","83","98","66","45","End of tournaments"]);
+
+
+basketballTournament(["Ballers","3","87","63","56","65","75","64","Sharks","4","64","76","65","86","68","99","45","78","End of tournaments"]);
+
