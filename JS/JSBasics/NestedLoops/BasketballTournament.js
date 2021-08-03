85cf652b5078756c1e12d7eb237be77de9612680
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-03 23:48:48 +0300

    Update BasketballTournament.js

--- JS/JSBasics/NestedLoops/BasketballTournament.js
+++ JS/JSBasics/NestedLoops/BasketballTournament.js
@@ -1,18 +1,32 @@
 function basketballTournament(params) {
-    let index = 0; 
-    let teamName = params[index];
-    
+    let index = 0;
+    let winCount = 0;
+    let lossCount = 0;
+    let totalGamesCount = 0;
 
-    while (condition) {
-        
+    while (params[index] !== "End of tournaments") {
+        if (isNaN(params[index])) { // team name 
+            for (let i = index + 1 ; i < Number(params[index + 1 * 2]); i++) { // 3 * 2 lines one our team, one the other 
+                if (i%2==0) {
+                    
+                }
+
+            }
+        }
+
+
+        index++;
+
     }
 
 
+    console.log(`${((winCount / totalGamesCount) * 100).toFixed(2)}% matches win`);
+    console.log(`${((lossCount / totalGamesCount) * 100).toFixed(2)}% matches lost`);
 
 }
 
-basketballTournament(["Dunkers","2","75","65","56","73","Fire Girls","3","67","34","83","98","66","45","End of tournaments"]);
+basketballTournament(["Dunkers", "2", "75", "65", "56", "73", "Fire Girls", "3", "67", "34", "83", "98", "66", "45", "End of tournaments"]);
 
 
-basketballTournament(["Ballers","3","87","63","56","65","75","64","Sharks","4","64","76","65","86","68","99","45","78","End of tournaments"]);
+basketballTournament(["Ballers", "3", "87", "63", "56", "65", "75", "64", "Sharks", "4", "64", "76", "65", "86", "68", "99", "45", "78", "End of tournaments"]);
 
