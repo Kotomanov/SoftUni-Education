6a482dd6bda70de9c0ca161b5534dab1bc9898c0
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-11 00:19:36 +0300

    Update EasterDecoration.js

--- JS/JSBasics/NestedLoops/EasterDecoration.js
+++ JS/JSBasics/NestedLoops/EasterDecoration.js
@@ -1,8 +1,8 @@
 function easterDecoration(params) {
     let customersCount = Number(params[0]);
-    let basketCount = 0;
-    let chocolateBunniesCount = 0;
-    let wreathCount = 0;
+    //let basketCount = 0;
+    // let chocolateBunniesCount = 0;
+    //let wreathCount = 0;
     let itemsCount = 0;
     let customerTotalAmount = 0;
     let totalSum = 0;
@@ -10,29 +10,33 @@ function easterDecoration(params) {
     for (let i = 1; i <= params.length; i++) {
 
         if (params[i] == "basket") {
-            basketCount++;
+            //basketCount++;
+            customerTotalAmount += 1.50;
             itemsCount++;
         } else if (params[i] == "wreath") {
-            wreathCount++;
+            //wreathCount++;
+            customerTotalAmount += 3.80;
             itemsCount++;
         }
         else if (params[i] == "chocolate bunny") {
-            chocolateBunniesCount++;
+            //chocolateBunniesCount++;
+            customerTotalAmount += 7;;
             itemsCount++;
         }
-       
-        customerTotalAmount = basketCount * 1.50 + chocolateBunniesCount * 7 + wreathCount * 3.90;
-            totalSum += customerTotalAmount;
-            if (itemsCount % 2 == 0) {
-                totalSum *= 0.80;
-            }
-         if (params[i] == "Finish") {
-            
+
+        //customerTotalAmount = basketCount * 1.50 + chocolateBunniesCount * 7 + wreathCount * 3.90;
+        totalSum += customerTotalAmount;
+        if (itemsCount % 2 == 0) {
+            totalSum *= 0.80;
+        }
+        if (params[i] == "Finish") {
+
             console.log(`You purchased ${itemsCount} items for ${customerTotalAmount} leva.`);
             customerTotalAmount = 0;
+            itemsCount = 0;
         }
 
-        
+
     }
 
     console.log(`Average bill per client is: ${(totalSum / customersCount).toFixed(2)} leva.`)
