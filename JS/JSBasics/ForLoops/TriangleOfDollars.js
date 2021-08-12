860916728c0610c808fb6d8ca20be88849875040
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-13 01:34:55 +0300

    Create TriangleOfDollars.js

---
+++ JS/JSBasics/ForLoops/TriangleOfDollars.js
@@ -0,0 +1,17 @@
+function triangleOfDollars(params) {
+    let number = Number(params[0]);
+
+    for (let row = 0; row < array.length; row++) {
+        for (let col = 0; col < array.length; col++) {
+            
+            
+        }
+        
+    }
+}
+
+triangleOfDollars(["2"]);
+
+triangleOfDollars(["3"]);
+
+triangleOfDollars(["8"]);
