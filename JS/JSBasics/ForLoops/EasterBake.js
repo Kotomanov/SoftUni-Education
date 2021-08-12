973f354bede3e0a037a918f7fad97d65a7b8d429
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-12 15:28:39 +0300

    Update EasterBake.js
    problem solved

--- JS/JSBasics/ForLoops/EasterBake.js
+++ JS/JSBasics/ForLoops/EasterBake.js
@@ -4,21 +4,30 @@ function easterBake(params) {
     let maxQtyFlour = -1;
     let sugarPackQty = 0;
     let flourPackQty = 0;
+    let totalSugarQty = 0;
+    let totalFlourQty = 0;
 
-    for (let b = 0; b < sweetBreadCount; b++) {
-        if (condition) { // sugar
-            
-        } else{ // flour
-
+    for (let b = 1; b < params.length; b++) {
+        if (b % 2 !== 0) { // sugar
+            totalSugarQty += Number(params[b]);
+            if (maxQtySugar < Number(params[b])) {
+                maxQtySugar = Number(params[b]);
+            }
+        } else { // flour
+            totalFlourQty += Number(params[b]);
+            if (maxQtyFlour < Number(params[b])) {
+                maxQtyFlour = Number(params[b]);
+            }
         }
-        
     }
 
+    sugarPackQty = Math.ceil(totalSugarQty / 950);
+    flourPackQty = Math.ceil(totalFlourQty / 750);
 
-    
-
+    console.log(`Sugar: ${sugarPackQty}`);
+    console.log(`Flour: ${flourPackQty}`);
+    console.log(`Max used flour is ${maxQtyFlour} grams, max used sugar is ${maxQtySugar} grams.`);
 }
-
 
 easterBake(["3", "400", "350", "250", "300", "450", "380"]);
 
