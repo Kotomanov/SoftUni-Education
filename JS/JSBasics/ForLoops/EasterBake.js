36944c94089e26bb064e3ac1888d746ab642a9dc
Miroslav Kotomanov <47686905+Kotomanov@users.noreply.github.com>
2021-08-12 15:09:41 +0300

    Update EasterBake.js

---
+++ JS/JSBasics/ForLoops/EasterBake.js
@@ -0,0 +1,26 @@
+function easterBake(params) {
+    let sweetBreadCount = Number(params[0]);
+    let maxQtySugar = -1;
+    let maxQtyFlour = -1;
+    let sugarPackQty = 0;
+    let flourPackQty = 0;
+
+    for (let b = 0; b < sweetBreadCount; b++) {
+        if (condition) { // sugar
+            
+        } else{ // flour
+
+        }
+        
+    }
+
+
+    
+
+}
+
+
+easterBake(["3", "400", "350", "250", "300", "450", "380"]);
+
+
+easterBake(["4", "500", "350", "560", "430", "600", "345", "578", "543"]);
